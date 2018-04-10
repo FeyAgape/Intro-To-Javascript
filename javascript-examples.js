e0dfcc18784ff64074e96f70b08dc7570734ed20
Fey Ijaware <fshola4real@gmail.com>
2018-04-10 17:17:12 +0100

    Update javascript-examples.js

--- javascript-examples.js
+++ javascript-examples.js
@@ -1,3 +1,100 @@
+// Logical operators can be used in conjunction with boolean values (true and false) to create complex logical expressions.
+// By combining two boolean values together with a logical operator, 
+// you create a logical expression that returns another boolean value. 
+// Here’s a description of the different logical operators:
+// &&  Logical AND value1 && value2  Returns true if both value1 and value2 evaluate to true.
+// ||  Logical OR  value1 || value2  Returns true if either value1 or value2 (or even both!) evaluates to true.
+// ! Logical NOT !value1 Returns the opposite of value1. If value1 is true, then !value1 is false.
+
+
+!(4 === 4) && "STRing" === "STRing"
+// Returns: false 
+
+//Example of an if statement using a logical operator
+var colt = "not busy";
+var weather = "nice";
+
+if (colt === "not busy" && weather === "nice") {
+  console.log("go to the park");
+}
+
+//Example of an if statement using a logical operator
+
+var dayOfTheWeeek = "Monday";
+var weather = "cloudy";
+
+if (weather === "cloudy" || dayOfTheWeeek === "Tuesday") {
+  console.log("Fey don't have to go to work");
+  } else if (weather === "cloudy") {
+  console.log("No Work");
+  } else if (dayOfTheWeeek === "Monday") {
+  console.log("Work");
+  }  else {
+  console.log("Work, Work, Work");
+}
+
+
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 0;
+var checkBalance = true;
+var isActive = true;
+
+// your code goes here
+if (checkBalance ===  false) {
+  console.log("Thank you. Have a nice day!");
+} else if (checkBalance ===  true || isActive === false ) {
+  console.log("Your acconut is no longer active");
+//} else if (checkBalance ===  true && balance > 0  ) {
+//  console.log("Your balance is $" + balance + ".");
+} else if (checkBalance ===  true && isActive === true && balance === 0  ) {
+  console.log("Your acconut is empty.");
+} else if (checkBalance ===  true && isActive === true && balance < 0  ) {
+  console.log("Your balance is negative. please contact bank.");
+}  else {
+  console.log("Your balance is $" + balance + ".");
+}
+
+
+var balance = 0;
+var checkBalance = true;
+var isActive = true;
+
+// your code goes here
+if (checkBalance ===  false) {
+  console.log("Thank you. Have a nice day!");
+} else if (checkBalance ===  true || isActive === true && balance === 0  ) {
+  console.log("Your acconut is empty.");
+} else if (checkBalance ===  true || isActive === false ) {
+  console.log("Your acconut is no longer active");
+//} else if (checkBalance ===  true && balance > 0  ) {
+//  console.log("Your balance is $" + balance + ".");
+} else if (checkBalance ===  true || isActive === true && balance < 0  ) {
+  console.log("Your balance is negative. please contact bank.");
+}  else {
+  console.log("Your balance is $" + balance + ".");
+}
+
+
+/// change the values of `flavor`, `vessel`, and `toppings` to test your code
+var flavor = "vanilla";
+var vessel = "cone";
+var toppings = "cookies";
+
+// Add your code here
+// Add your code here
+
+if (flavor ===  ("vanilla" || "chocolate") || vessel === ("cone" || "bowl")  || toppings === ("sprinkles" || "peanuts") ) {
+  console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings + ".");
+}  else {
+  console.log("No Ice cream");
+}
+
+
+
+
+
+
 //Example of an else if statement
 // change the value of `weather` to test your conditional statements
 var weather = "snow";
@@ -8,7 +105,6 @@ if (weather === "snow") {
 } else {
   console.log("Wear what you have on.");
 }
-
 
 //Example of an if else statement
 var a = 1;
